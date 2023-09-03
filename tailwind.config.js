993936a27a69dae3dd3a11d5cbce0aaeb30e3bc5
Tnchnd <52105776+tnchnd@users.noreply.github.com>
2023-09-03 22:04:32 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        ijo: "#16a34a",
+        gelap: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
